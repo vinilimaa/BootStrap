9af97cc2b52784e6b52a3149bee53b18e4fd18df
EMANOEL VINICIUS MENEZES DE LIMA <84748132+vinilimaa@users.noreply.github.com>
2021-08-20 11:31:03 -0300

    Valor do pedido
    A aplicação agora informa o valor total do pedido feito.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let prato, bebida, sobremesa;
+
+
+let prato, bebida, sobremesa, p1, p2, p3, soma;
 
 
 function escolherFarofa() {
@@ -7,7 +9,7 @@ document.getElementById("farofa").style.border = "2px solid green";
 document.getElementById("arroz").style.border = "white";
 document.getElementById("prato").style.border = "white";
 prato="Farofa";
-
+p1 = 10;
 
 }
 
@@ -16,7 +18,7 @@ function escolherArroz () {
     document.getElementById("arroz").style.border = "2px solid green";
     document.getElementById("prato").style.border = "2px solid white";
     prato = "Arroz";
-   
+   p1 = 12;
     
 
 }
@@ -26,6 +28,7 @@ function escolherPrato () {
     document.getElementById("arroz").style.border = "2px solid white";
     document.getElementById("prato").style.border = "2px solid green";
     prato = "Prato-feito";
+    p1 = 10;
    
 
 
@@ -36,7 +39,7 @@ function escolherSuco () {
     document.getElementById("coca").style.border = "2px solid white";
     document.getElementById("chopp").style.border = "2px solid white";
     bebida = "Suco";
-   
+   p2 = 5;
 
 }
 
@@ -46,6 +49,7 @@ function escolherCoca () {
     document.getElementById("coca").style.border = "2px solid green";
     document.getElementById("chopp").style.border = "2px solid white";
     bebida = "Coca-Cola"
+    p2 = 3;
     
 }
 
@@ -55,6 +59,7 @@ function escolherChopp () {
     document.getElementById("chopp").style.border = "2px solid green";
     document.getElementById("coca").style.border = "2px solid white";
     bebida = "Chopp"
+    p2 = 7;
     
 }
 
@@ -64,6 +69,7 @@ function escolherPudim(){
     document.getElementById("sorvete").style.border = "2px solid white";
     document.getElementById("cup").style.border = "2px solid white";
     sobremesa = "Pudim";
+    p3 = 4.50;
     
 }
 
@@ -72,6 +78,7 @@ function escolherCup(){
     document.getElementById("sorvete").style.border = "2px solid white";
     document.getElementById("pudim").style.border = "2px solid white";
     sobremesa = "Cupcake";
+    p3 = 4.5
     
 }
 
@@ -80,14 +87,19 @@ function escolherSorvete (){
     document.getElementById("sorvete").style.border = "2px solid green";
     document.getElementById("cup").style.border = "2px solid white";
     sobremesa = "Sorvete";
+    p3 = 2;
     
 
 }
 
+
+
+
 function finalizarPedido () {
     let msg;
-    msg = "Eu gostaria de pedir: " + prato  + ", " + bebida + " e " + sobremesa +".";
-    window.open("https://wa.me/+5587999999999?text=" + msg);
+    let soma = p1 + p2 + p3;
+    msg = "Eu gostaria de pedir: " + prato  + ", " + bebida + " e " + sobremesa +". " +"Totalizando = " + " R$ ";
+    window.open("https://wa.me/+5587999999999?text=" + msg + soma);
    
    
 }
